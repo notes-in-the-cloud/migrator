638f5bafc12ff1fea224eca41ffd8fe1bd10a871
Hristo Partenov <hristokubev@gmail.com>
2026-05-05 17:03:12 +0300

    fix migrations

--- migrations/20260502075506_initial_auth_service_users.up.sql
+++ migrations/20260502075506_initial_auth_service_users.up.sql
@@ -4,8 +4,7 @@ CREATE SCHEMA IF NOT EXISTS auth_service;
 
 CREATE TABLE auth_service.users (
     id uuid PRIMARY KEY,
-    first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
+    display_name varchar(255) NOT NULL,
     email varchar(255) NOT NULL UNIQUE,
     password_hash text NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
